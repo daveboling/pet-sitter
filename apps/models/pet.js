07fdefa78c0c088b58be3d11fd8c703477b15835
David <kadowki@gmail.com>
2014-07-16 14:55:14 -0500

    Added some more testing and some additional pet functions

--- apps/models/pet.js
+++ apps/models/pet.js
@@ -3,16 +3,40 @@ function Pet(name, age, gender, species){
 	this.age     = age;
 	this.gender  = gender;
 	this.species = species;
+	this.isZombie  = false;
 
-	this.health = Math.random()*91+10;
-	this.energy = Math.random()*91+10;
-	this.full   = Math.random()*91+10;
+	this.health = Math.floor(Math.random()*91+10);
+	this.energy = Math.floor(Math.random()*91+10);
+	this.full   = Math.floor(Math.random()*91+10);
 }
 
 Pet.prototype.walk = function() {
-	this.health +=  Math.round((Math.random()*2)+1);
-	this.energy -=  Math.round((Math.random()*2)+2);
-	this.full   -=  Math.round((Math.random()*3)+2);
+	this.health +=  Math.floor((Math.random()*3)+1);
+	this.energy -=  Math.floor((Math.random()*3)+2);
+	this.full   -=  Math.floor((Math.random()*3)+3);
 }
+
+Pet.prototype.sleep = function() {
+	this.health +=  Math.floor((Math.random()*2)+2);
+	this.energy +=  Math.floor((Math.random()*5)+1);
+	this.full   -=  Math.floor((Math.random()*4)+4);
+}
+
+Pet.prototype.eat = function() {
+	this.health +=  Math.floor((Math.random()*4)+1);
+	this.energy -=  Math.floor((Math.random()*6)+3);
+	this.full   +=  Math.floor((Math.random()*5)+5);
+}
+
+Pet.prototype.attack = function(Pet) {
+	var damage = Math.floor((this.health/10) + (this.energy/20) + (this.full/30));
+		damage += Math.random()*5;
+	Pet.health -= damage;
+
+	if(Pet.health < 0){
+		Pet.isZombie = true;
+	}
+};
+
 
 module.exports = Pet;
--- test/unit/pet.js
+++ test/unit/pet.js
@@ -7,6 +7,7 @@ var Pet = require('../../apps/models/pet'); //bring in for testing
 
 //these are known as describe blocks, a way to organize tests
 describe('Pet', function(){
+	//Test to see if valid constructor
 	describe('constructor', function() {
 		//Test the pet object to see if it is indeed and object
 		it('should create a new Pet', function() {
@@ -21,12 +22,14 @@ describe('Pet', function(){
 			expect(pet.age).to.equal(3);
 			expect(pet.gender).to.equal('female');
 			expect(pet.species).to.equal('lizard');
+			expect(pet.isZombie).to.be.false;
 			//Heal, energy, full test
 			expect(pet.health).to.within(10, 100);
 			expect(pet.energy).to.within(10, 100);
 			expect(pet.full).to.within(10, 100);
 		});
 	});
+	//Test the walk function
 	describe('#walk', function() {
 		it('should increase health but decrease energy and full', function() {
 			var pet = new Pet('fluffy', 3, 'female', 'lizard');
@@ -38,12 +41,85 @@ describe('Pet', function(){
 			pet.walk();
 
 			//Log out the new numbers
-			console.log(pet.health, pet.energy, pet.full);
+			console.log('Health', pet.health, 'Energy', pet.energy,'Full', pet.full);
 
 			//Check to see if the properties are within proper values
 			expect(pet.health).to.be.within(91, 93);
 			expect(pet.energy).to.be.within(26, 28);
-			expect(pet.full).to.be.within(15, 17);
+			expect(pet.full).to.be.within(15, 17); 
+		});
+	});
+	//Test the sleep function
+	describe('#sleep', function() {
+		it('should increase health, energy and decrease full', function() {
+			var pet = new Pet('fluffy', 3, 'female', 'lizard');
+			pet.health = 90;
+			pet.energy = 30;
+			pet.full   = 20;
+
+			//Call function
+			pet.sleep();
+
+			//Log out the new numbers
+			console.log('Health', pet.health, 'Energy', pet.energy,'Full', pet.full);
+
+			//Check to see if the properties are within proper values
+			expect(pet.health).to.be.within(92, 93);
+			expect(pet.energy).to.be.within(31, 35);
+			expect(pet.full).to.be.within(13 ,16);
+
+		});
+	});
+	//Test the eat function
+	describe('#eat', function() {
+		it('should increase full and health, but decrease energy', function() {
+			var pet = new Pet('fluffy', 3, 'female', 'lizard');
+			pet.health = 90;
+			pet.energy = 30;
+			pet.full   = 20;
+
+			//Call function
+			pet.eat();
+
+			//Log out the new numbers
+			console.log('Health', pet.health, 'Energy', pet.energy,'Full', pet.full);
+
+			//Check to see if the properties are within proper values
+			expect(pet.health).to.be.within(91, 94);
+			expect(pet.energy).to.be.within(22, 27);
+			expect(pet.full).to.be.within(25, 29);
+
+		});
+	});
+	describe('#attack', function() {
+		it('should allow pets to cage fight', function() {
+			var fluffy = new Pet('fluffy', 3, 'female', 'lizard');
+			fluffy.health = 45;
+			fluffy.energy = 60;
+			fluffy.full   = 90;
+
+			var dax = new Pet('dax', 7, 'female', 'slug');
+			dax.health = 70;
+			dax.energy = 50;
+			dax.full   = 80;
+
+			//Call function
+			fluffy.attack(dax);
+
+			//fluffy's attack is at least 10.5 and at most 15.5
+			//dax - minAttack, dax - maxAttack
+			expect(dax.health).to.be.within(54.5, 59.5);
+
+		});
+		it('should cause a pet  with negative health to become zombie', function() {
+			var fluffy = new Pet('fluffy', 3, 'female', 'lizard');
+			var dax = new Pet('dax', 7, 'female', 'slug');
+
+			for(var i = 0; i < 1000; i++){
+				fluffy.attack(dax);
+			}
+
+			expect(dax.isZombie).to.be.true;
 
 		});
 	});
